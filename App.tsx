2259c1a430304ab6381b537d2f65e44e37582129
Scott Yinan Fan <scottvan2008@gmail.com>
2025-04-11 02:07:05 +0800

    v1.0

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState, useCallback } from "react";
+import { StyleSheet, RefreshControl, Platform, ScrollView } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(): JSX.Element {
+    const webviewRef = useRef<any>(null);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        webviewRef.current?.reload();
+        setTimeout(() => setRefreshing(false), 1000); // wait a bit before ending refresh UI
+    }, []);
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView
+                style={styles.container}
+                edges={["top", "left", "right"]}
+            >
+                {Platform.OS === "ios" ? (
+                    <ScrollView
+                        contentContainerStyle={{ flex: 1 }}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={refreshing}
+                                onRefresh={onRefresh}
+                            />
+                        }
+                    >
+                        <WebView
+                            ref={webviewRef}
+                            source={{ uri: "https://www.coinwait.com" }}
+                            javaScriptEnabled
+                            domStorageEnabled
+                        />
+                    </ScrollView>
+                ) : (
+                    <WebView
+                        ref={webviewRef}
+                        source={{ uri: "https://www.coinwait.com" }}
+                        pullToRefreshEnabled={true}
+                        onRefresh={onRefresh}
+                        javaScriptEnabled
+                        domStorageEnabled
+                    />
+                )}
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
 });
